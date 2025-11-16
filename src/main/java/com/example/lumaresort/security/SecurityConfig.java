db7729f47d76542671411ab195421c666ebf2cce
Santcar05 <castrogarzonsantiago@gmail.com>
2025-11-16 18:11:26 -0500

    CORS solucionado

--- src/main/java/com/example/lumaresort/security/SecurityConfig.java
+++ src/main/java/com/example/lumaresort/security/SecurityConfig.java
@@ -139,7 +139,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000", "http://localhost:3000", "https://jazzy-custard-700f04.netlify.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
